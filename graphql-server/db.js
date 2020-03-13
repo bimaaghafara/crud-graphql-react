6605513b53fb57a4a5ff8af5506b43715db0f6d3
bimaaghafara <bimaa7@gmailcom>
2020-03-13 18:07:37 +0700

    .

---
+++ graphql-server/db.js
@@ -0,0 +1,16 @@
+module.exports = {
+    users: [
+        { id: 1, username: 'john_anto', firstName: "john", lastName: "anto" },
+        { id: 2, username: 'mikael_bedu', firstName: "mikael", lastName: "bedu" },
+        { id: 3, username: 'arthur_cepi', firstName: "arthur", lastName: "cepi" },
+        { id: 4, username: 'angel_dian', firstName: "angel", lastName: "dian" },
+        { id: 5, username: 'brian_eko', firstName: "brian", lastName: "eko" }
+    ],
+    posts: [
+        { id: 1, title: "Sir Dolor amet", desc: "Description 1", user_id: 1 },
+        { id: 2, title: "Sic Dolor amet", desc: "Description 2", user_id: 2 },
+        { id: 3, title: "Sic Dolor amet", desc: "Description 3", user_id: 3 },
+        { id: 4, title: "Sic Dolor amet", desc: "Description 4", user_id: 4 },
+        { id: 5, title: "Sic Dolor amet", desc: "Description 5", user_id: 5 },
+    ],
+}
