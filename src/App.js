0379eab4d95e8ca4212f5b618900b56649007414
bimaaghafara <bimaa7@gmailcom>
2020-03-13 19:27:45 +0700

    route in app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// styles
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// pages
+
+
+// router
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Home() {return (<div>Home</div>)}
+function User() {return (<div>User</div>)}
+function Post() {return (<div>Post</div>)}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        activeMenu: 'home'
+    };
+  }
+
+  render() {
+
+    const Menu = ({ menu }) => (
+      <li style={{'paddingTop': '8px'}}>
+        <Link 
+          to={menu.link}
+          onClick={() => {
+            this.setState({activeMenu: menu.key}
+          )}}
+          className={'nav btn btn-' + (this.state.activeMenu===menu.key? 'success' : 'default')}
+          >{menu.label}</Link>
+      </li>
+    );
+
+    return (
+      <div className="App">
+        <Router>
+          <div style={{'padding': '25px 50px'}}>
+            <h4>Please Select One Menu:</h4>
+            <ul>
+              <Menu menu={{key:"home", label:"Home", link:"/"}}/>
+              <Menu menu={{key:"user", label:"User", link:"/user"}}/>
+              <Menu menu={{key:"post", label:"Post", link:"/post"}}/>
+            </ul>
+            <hr />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/user" component={User} />
+            <Route exact path="/post" component={Post} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
